5460acff99dc0cbf1a09d80f9cd28eb8b75997f4
Alvaro <ahc_frisdal@hotmail.com>
2017-12-11 16:19:45 +0100

    Añadidos layouts

--- PokerChips/app/src/main/java/galvan/pokerchips/GameActivity.java
+++ PokerChips/app/src/main/java/galvan/pokerchips/GameActivity.java
@@ -4,21 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
     // Añado un comentario para probar el push + update...
 
-    private Button btn_plus_chip = (Button) findViewById(R.id.btn_plus_chip);
+    //private Button btn_plus_chip = (Button) findViewById(R.id.btn_plus_chip);   va en el onCreate despues de crear el layout
     private int[] chips ={5,10,25,50,100};
     private int pos;
     private TextView txt_chip ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //lista predeterminada solo de nombres
+        ListView player_list = (ListView)findViewById(R.id.players_list);
+        String[] values = new String[]{"Menganito","Fulanito","Juanito","Estalactito","Teodoro","Pauek","Eustaquio"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        player_list.setAdapter(adapter);
+
+        Button btn_plus_chip = (Button) findViewById(R.id.btn_plus_chip);
 
         pos=0;
         txt_chip =(TextView) findViewById(R.id.txt_chip);
---
+++ PokerChips/app/src/main/java/galvan/pokerchips/PlayerListActivity.java
@@ -0,0 +1,25 @@
+package galvan.pokerchips;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Alvaro on 11/12/2017.
+ */
+
+public class PlayerListActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playerlist);
+
+        ListView player_list = (ListView)findViewById(R.id.players_list);
+        String[] values = new String[]{"Menganito","Fulanito","Juanito","Estalactito","Teodoro","Pauek","Eustaquio"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        player_list.setAdapter(adapter);
+
+
+    }
+}
