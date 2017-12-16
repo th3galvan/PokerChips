3353dbb7025ab5445aabf39eb874bd087796319d
th3galvan <galvan30396@gmail.com>
2017-12-16 16:17:11 +0100

    introduccion parametros firebase + clase Player

--- PokerChips/app/src/main/java/galvan/pokerchips/Player.java
+++ PokerChips/app/src/main/java/galvan/pokerchips/Player.java
@@ -1,12 +1,13 @@
 package galvan.pokerchips;
 
 /**
+<<<<<<< HEAD
  * Created by Alvaro on 11/12/2017.
  *
  * Esta sin uso, la empece pero decidi usar PlayerItems mejor
  */
 
-public class Player {
+public class PlayerGalvan {
     private  String name;
     private int chips;
     private boolean dealer;
@@ -35,4 +36,117 @@ public class Player {
     }
 
 
+=======
+ * Created by Galvan on 15/12/2017.
+ */
+
+public class Player {
+    private String name;
+    private  int chips;
+    private  int bet;
+
+    private  boolean big;
+    private  boolean small;
+    private  boolean dealer;
+
+    private  boolean out;
+    private  boolean turn;
+    private  boolean allin;
+    public Player(){}
+
+    public Player(String name,
+                  int chips,
+                  int bet,
+                  boolean big,
+                  boolean small,
+                  boolean dealer,
+                  boolean out,
+                  boolean turn,
+                  boolean allin;){
+        this.name = name;
+        this.bet = bet;
+        this.big = big;
+        this.small = small;
+        this.dealer = dealer;
+        this.out = out;
+        this.turn = turn;
+        this.allin = allin;
+
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public boolean isBig() {
+        return big;
+    }
+
+    public boolean isSmall() {
+        return small;
+    }
+
+    public boolean isDealer() {
+        return dealer;
+    }
+
+    public boolean isOut() {
+        return out;
+    }
+
+    public boolean isTurn() {
+        return turn;
+    }
+
+    public boolean isAllin() {
+        return allin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setChips(int chips) {
+        this.chips = chips;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public void setBig(boolean big) {
+        this.big = big;
+    }
+
+    public void setSmall(boolean small) {
+        this.small = small;
+    }
+
+    public void setDealer(boolean dealer) {
+        this.dealer = dealer;
+    }
+
+    public void setOut(boolean out) {
+        this.out = out;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+
+    public void setAllin(boolean allin) {
+        this.allin = allin;
+    }
+>>>>>>> introduccion parametros firebase + clase Player
 }
