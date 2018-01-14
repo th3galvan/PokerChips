4df7949bd12ac547cfdef7819c391e04d7987e17
Alvaro <ahc_frisdal@hotmail.com>
2018-01-14 07:15:49 +0100

    Función generadora de código QR en ShowCodeActivity, aprovecha el código númerico que hay y lo transforma en QR Adabtado el layout de showqr para añadir imageView para el QR

--- PokerChips/app/src/main/java/galvan/pokerchips/ShowCodeActivity.java
+++ PokerChips/app/src/main/java/galvan/pokerchips/ShowCodeActivity.java
@@ -1,12 +1,21 @@
 package galvan.pokerchips;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 
 /**
  * Created by xvilaseca on 09/01/2018.
@@ -25,6 +34,8 @@ public class ShowCodeActivity extends AppCompatActivity {
     private int change_value_big;
 
     private String name;
+    private ImageView image;
+
 
 
     @Override
@@ -45,7 +56,7 @@ public class ShowCodeActivity extends AppCompatActivity {
 
         Button btn_code = (Button)findViewById(R.id.btn_generate_code);
         txt_code = (TextView)findViewById(R.id.txt_code);
-
+        image = (ImageView) findViewById(R.id.image);
 
 
         btn_code.setOnClickListener(new View.OnClickListener() {
@@ -55,19 +66,28 @@ public class ShowCodeActivity extends AppCompatActivity {
                 String string_code = Integer.toString(code);
                 txt_code.setText(string_code);
 
-                Intent intent_list = new Intent (ShowCodeActivity.this, PlayersListActivity.class);
-                intent_list.putExtra("name",name);
-                intent_list.putExtra("playersnumber",number_players);
-                intent_list.putExtra("initial_chips",initial_chips);
-                intent_list.putExtra("bigblind",big);
-                intent_list.putExtra("frecuency",time_big_up);
-                intent_list.putExtra("change",change_value_big);
+                Intent intent_list = new Intent(ShowCodeActivity.this, PlayersListActivity.class);
+                intent_list.putExtra("name", name);
+                intent_list.putExtra("playersnumber", number_players);
+                intent_list.putExtra("initial_chips", initial_chips);
+                intent_list.putExtra("bigblind", big);
+                intent_list.putExtra("frecuency", time_big_up);
+                intent_list.putExtra("change", change_value_big);
                 startActivity(intent_list);
 
+                //funcion de generar QR
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(string_code, BarcodeFormat.QR_CODE, 300, 300);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
-
     }
-
 }
+
+
